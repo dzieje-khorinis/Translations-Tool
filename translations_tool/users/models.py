efd1c33da0df8828ec4ff3c5e6bdd575f19d71a2
Krzysztof Jura <kisioj@gmail.com>
2021-01-17 20:47:52 +0100

    added basic models structure

--- translations_tool/users/models.py
+++ translations_tool/users/models.py
@@ -1,7 +1,11 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, ForeignKey
+from django.db.models.deletion import SET_NULL
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+
+from translations_tool.translations.models import Translation
 
 
 class User(AbstractUser):
@@ -9,6 +13,78 @@ class User(AbstractUser):
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
+
+    chief = ForeignKey("self", on_delete=SET_NULL, blank=True, null=True, related_name="subordinates")
+    ADMIN = "ADMIN"
+    COORDINATOR = "COORDINATOR"
+    TRANSLATOR = "TRANSLATOR"
+    ROLE = [
+        (ADMIN, _("Admin")),
+        (COORDINATOR, _("Coordinator")),
+        (TRANSLATOR, _("Translator")),
+    ]
+    role = CharField(max_length=255, choices=ROLE, blank=True)
+    role_related_language = CharField(max_length=2, choices=settings.LANGUAGES, blank=True)
+
+    def is_admin(self):
+        return self.is_superuser or self.role == User.ADMIN
+
+    def get_languages(self):
+        languages = dict(settings.LANGUAGES)
+        if not self.role:
+            return []
+
+        if self.role in (User.COORDINATOR, User.TRANSLATOR) and not self.is_superuser:
+            return [(self.role_related_language, languages[self.role_related_language])]
+
+        return settings.LANGUAGES
+
+    def get_translation_language(self, lang_code):
+        if self.role in (User.COORDINATOR, User.TRANSLATOR) and not self.is_superuser:
+            return self.role_related_language
+        return lang_code or settings.LANGUAGE_CODE
+
+    def get_state_actions(self):
+        actions = []
+
+        if not self.role:
+            return actions
+
+        actions.extend(
+            [
+                Translation.TODO,
+                Translation.READY_TO_REVIEW,
+            ]
+        )
+        if self.role == User.TRANSLATOR and not self.is_superuser:
+            return actions
+
+        actions.extend(
+            [
+                Translation.NEEDS_WORK,
+                Translation.ACCEPTED,
+            ]
+        )
+
+        if self.role == User.COORDINATOR and not self.is_superuser:
+            return actions
+
+        actions = [Translation.NEW] + actions
+        return actions
+
+    def get_role(self):
+        if self.is_superuser:
+            return _("Super Admin")
+
+        if not self.role:
+            return _("None")
+
+        role_code_to_value = dict(User.ROLE)
+        role_pretty = role_code_to_value[self.role]
+        if self.role in (User.COORDINATOR, User.TRANSLATOR) and self.role_related_language:
+            role_pretty = f"{role_pretty} [{self.role_related_language.upper()}]"
+
+        return role_pretty
 
     def get_absolute_url(self):
         """Get url for user's detail view.
