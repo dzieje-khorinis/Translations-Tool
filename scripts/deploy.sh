f24a7712eeff9df7d2d40cd9564d496729875e94
Artur <arturmiks@gmail.com>
2021-12-27 13:55:56 +0100

    Get rid off deployment logic from continues integration

--- scripts/deploy.sh
+++
@@ -1,3 +0,0 @@
-cd /home/deploy-user/Translations-Tool
-cat TOKEN.txt | docker login ghcr.io -u arturkasperek --password-stdin
-docker stack deploy -c stack.yml --with-registry-auth translation-tool-prod
